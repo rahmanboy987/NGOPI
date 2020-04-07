67b4965cda0de08129667cd584435d5ffe963f57
Moh. Fathur Rohman <rahmanboy987@gmail.com>
2020-04-07 07:03:41 +0700

    Update NGOPI

---
+++ asset/database/NGOPI.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 07, 2020 at 01:52 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `NGOPI`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_keluar`
+--
+
+CREATE TABLE `detail_keluar` (
+  `id_detail_keluar` int(11) NOT NULL,
+  `id_keluar` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `jumlah_keluar` int(11) NOT NULL,
+  `subtotal_keluar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_masuk`
+--
+
+CREATE TABLE `detail_masuk` (
+  `id_detail_masuk` int(11) NOT NULL,
+  `id_masuk` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `jumlah_masuk` int(11) NOT NULL,
+  `jumlah_rusak` int(11) NOT NULL,
+  `subtotal_masuk` int(11) NOT NULL,
+  `subtotal_rusak` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gudang`
+--
+
+CREATE TABLE `gudang` (
+  `id_barang` int(11) NOT NULL,
+  `jenis_barang` varchar(64) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `stock_barang` int(11) NOT NULL,
+  `harga_jual` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian_masuk`
+--
+
+CREATE TABLE `pembelian_masuk` (
+  `id_masuk` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nama_toko` varchar(255) NOT NULL,
+  `waktu_masuk` datetime NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  `total_rugi` int(11) NOT NULL,
+  `is_cek` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan_keluar`
+--
+
+CREATE TABLE `penjualan_keluar` (
+  `id_keluar` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `waktu_keluar` datetime NOT NULL,
+  `total_harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `phone` varchar(16) NOT NULL,
+  `ktp` varchar(32) NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_keluar`
+--
+ALTER TABLE `detail_keluar`
+  ADD PRIMARY KEY (`id_detail_keluar`);
+
+--
+-- Indexes for table `detail_masuk`
+--
+ALTER TABLE `detail_masuk`
+  ADD PRIMARY KEY (`id_detail_masuk`);
+
+--
+-- Indexes for table `gudang`
+--
+ALTER TABLE `gudang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `pembelian_masuk`
+--
+ALTER TABLE `pembelian_masuk`
+  ADD PRIMARY KEY (`id_masuk`);
+
+--
+-- Indexes for table `penjualan_keluar`
+--
+ALTER TABLE `penjualan_keluar`
+  ADD PRIMARY KEY (`id_keluar`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail_keluar`
+--
+ALTER TABLE `detail_keluar`
+  MODIFY `id_detail_keluar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detail_masuk`
+--
+ALTER TABLE `detail_masuk`
+  MODIFY `id_detail_masuk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gudang`
+--
+ALTER TABLE `gudang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pembelian_masuk`
+--
+ALTER TABLE `pembelian_masuk`
+  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `penjualan_keluar`
+--
+ALTER TABLE `penjualan_keluar`
+  MODIFY `id_keluar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
