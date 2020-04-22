0588c75049c3d4d15ba8467d40668b13080038d9
faradella <42823922+faradella@users.noreply.github.com>
2020-04-22 19:42:41 +0700

    Update menu.php

--- application/views/home/menu.php
+++ application/views/home/menu.php
@@ -1,1 +1,23 @@
+<section class="page-section cta">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <div class="cta-inner text-center rounded">
+            <h4 class="section-heading mb-4">
+            <table class="table">
+            <div class="table-responsive">
+              <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Menu</th>
+                  <th>Harga</th>
+                  <th>Pilihan</th>
+                </tr>
+              </thead>
+              </table>
+            </div>
+          </table>
+          </h4>
+  </section>
 
