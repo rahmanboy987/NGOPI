b4d75f6ce6433894e3075038b4497bfa877f4c4b
Fathur <rahmanboy987@gmail.com>
2020-05-30 18:58:45 +0700

    update

--- application/views/admin/settings.php
+++ application/views/admin/settings.php
@@ -1,6 +1,5 @@
 <div class="container-fluid">
     <h1 class="h3 mb-2 text-gray-800">Settings</h1>
-    <p class="mb-4">Tempat dimana admin bisa mensetting page utama.</p>
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Main Settings</h6>
--- application/views/admin/user.php
+++ application/views/admin/user.php
@@ -1,6 +1,5 @@
 <div class="container-fluid">
   <h1 class="h3 mb-2 text-gray-800">User</h1>
-  <p class="mb-4">User adalah seorang karyawan ataupun pemilik warkop tersebut.</p>
   <div class="card shadow mb-4">
     <div class="card-header py-3">
       <h6 class="m-0 font-weight-bold text-primary">DataTable User</h6>
